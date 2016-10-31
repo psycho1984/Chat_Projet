19cab6b0a676d2018b0b27620909d09ca17082c2
avi <aviledovsky@gmail.com>
2016-10-31 02:54:54 -0700

    Initial Commit

---
+++ Chat_Projet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat_Projet
+//
+//  Created by aviledovsky on 31/10/2016.
+//  Copyright © 2016 aviledovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
